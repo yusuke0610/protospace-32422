aa3669703bdce4edc1eb581f630d78089034af60
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2020-12-08 10:02:42 +0900

    ログイン、ログアウト、データ保存確認

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,17 @@
 class PrototypesController < ApplicationController
+  
   def index
+   @user = User.all
+    
   end
+  def new
+    @user = User.new
+  end
+  
+  def create
+    User.create(user_params)
+  end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  # resources :users only: [:index ,:new ,:create ,:destroy] 
+  resources :prototypes
+
   root to: "prototypes#index"
+  get "/users/sign_up", to: "devise/registrations#new"
+  post "/users/" ,to: "devise/registrations#create"
+
+  get "/users/sign_in", to: "devise/sessions#new"
+  post "/users" ,to: "devise/sessions#create"
+  get "/users/sign_out", to: "devise/sessions#destroy"
+  
 end
